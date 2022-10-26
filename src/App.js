cf38b024aba1e9e3bc396b3aa4568f8854cd384d
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-10-27 01:11:06 +0200

    Add to the table feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Component} from 'react';
 import './App.css';
+import Map from './Components/Map';
+import 'leaflet/dist/leaflet.css';
+import { nanoid } from '@reduxjs/toolkit';
 
 
 class App extends Component {
@@ -9,55 +12,146 @@ class App extends Component {
       "deaths" :   {"value": 300} 
     }],
     countryList: {
-      "countries" : [
-                      {
-                        "name" : "testCountry",
-                        "iso3" : "tc"
-                      }
-                    ]
+      "countries" : [{"name":"First Country", "iso3": "FC"}]
+    },
+    addData : {
+      "data" : [{"confirmed": {"value": 0}, "deaths": {"value": 0}}],
+      "countryList" : {"countries" : [{"name":"", "iso3":""}]}
     }
   }
   LoadEveryCountryData = () => {
-    this.setState({ data: [{
-      "confirmed" : {"value": 300}, 
-      "deaths" :   {"value": 300} 
-    }] });
-    this.state.countryList.countries.map(country => this.getCountryData(country.name));
-  }
-  ResetTableToInitialValue= () => {
-    this.setState({ data: [{
-      "confirmed" : {"value": 300}, 
-      "deaths" :   {"value": 300} 
-    }] });
-  }
-  componentDidMount(){
-    // fetch Countries from Country API
+    this.setState({ countryList : []})
+    this.setState({ data: [] });
+    //load all countries
     fetch('https://covid19.mathdro.id/api/countries')
-
     .then((response) => response.json())
-
     .then(countryList => {
         this.setState({ countryList: countryList });
     });
-    
+    //get corresponsing data
+    this.state.countryList.countries.map(country => this.getCountryData(country.name));
+  }
 
+  ResetTableToInitialValue= () => {
+    this.setState({ data: [] }); 
+    this.setState({countryList: {"countries":[]}});
+  }
+
+  componentDidMount(){
+    
   }
 // retrieving data from every country
 
  getCountryData = async (country) => {
   const response = await fetch('https://covid19.mathdro.id/api/countries/' + country);
   const result = response.json();
+  this.setState({data : [...this.state.data, await result]});
+}
+
+handleAddCovidData = (event) => {
+  event.preventDefault();
+  const fieldName = event.target.getAttribute('name');
+  const fieldValue = event.target.value;
+
+
+if(fieldName==='confirmed'){
+  // 1. Make a shallow copy of the items
+  let addData = {...this.state.addData};
+  // 2. Make a shallow copy of the item you want to mutate
+  let data = {...addData.data[0]};
+  // 3. Replace the property you're intested in
+  data.confirmed.value = fieldValue;
+  // 4. Put it back into our array. N.B. we *are* mutating the array here, 
+  //    but that's why we made a copy first
+  addData.data[0] = data;
+  // 5. Set the state to our new copy
+  this.setState({addData: addData});
   
-  this.setState({data : [...this.state.data, await result /* {
-    "confirmed" : await result.confirmed,/* {"value": 300}, 
-    "deaths" :  await result.deaths/* {"value": 300} 
-  } */]});
+}else if (fieldName==='deaths'){
+  let addData = {...this.state.addData};
+  // 2. Make a shallow copy of the item you want to mutate
+  let data = {...addData.data[0]};
+  // 3. Replace the property you're intested in
+  data.deaths.value = fieldValue;
+  // 4. Put it back into our array. N.B. we *are* mutating the array here, 
+  //    but that's why we made a copy first
+  addData.data[0] = data;
+  // 5. Set the state to our new copy
+  this.setState({addData: addData});
+}
+
+}
+
+handleAddCountry = (event) => {
+  event.preventDefault();
+  const fieldName = event.target.getAttribute('name');
+  const fieldValue = event.target.value;
+
+
+  if(fieldName==='name'){
+    // 1. Make a shallow copy of the items
+    let addData = {...this.state.addData};
+    // 2. Make a shallow copy of the item you want to mutate
+    let data = {...addData.countryList.countries[0]};
+    // 3. Replace the property you're intested in
+    data.name = fieldValue;
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, 
+    //    but that's why we made a copy first
+    addData.countryList.countries[0] = data;
+    // 5. Set the state to our new copy
+    this.setState({addData: addData});
+    
+  }else if (fieldName==='iso3'){
+    let addData = {...this.state.addData};
+    // 2. Make a shallow copy of the item you want to mutate
+    let data = {...addData.countryList.countries[0]};
+    // 3. Replace the property you're intested in
+    data.iso3 = fieldValue;
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, 
+    //    but that's why we made a copy first
+    addData.countryList.countries[0] = data;
+    // 5. Set the state to our new copy
+  }
+
+}
+handleAddData = (event) => {
+  event.preventDefault();
+
+  const dataArray = {"confirmed":{"value": this.state.addData.data[0].confirmed.value}, 'deaths': {"value": this.state.addData.data[0].deaths.value}, id: nanoid()};
+  const countryObject = {"name":this.state.addData.countryList.countries[0].name, "iso3": this.state.addData.countryList.countries[0].iso3}; 
+  this.setState({data: [...this.state.data, dataArray]})
+  this.setState({countryList: {"countries" : [...this.state.countryList.countries, countryObject]}});
+  console.log(this.state.addData);
+  console.log(this.state.data);
+  console.log(this.state.countryList);
+
+  /* this.setState({addData: {
+    "data" : [{"confirmed": 0, "deaths": 0}],
+    "countryList" : {"countries" : [{"name":"", "iso3":""}]}
+  } }) */
+  
 }
 render (){
   return (
     <div className="app-container">
-      <button onClick={this.LoadEveryCountryData}>Load Every Country Data from API</button>
-      <button onClick={this.ResetTableToInitialValue}>Rest Table to test</button>
+      <div className='buttons'>
+        <button onClick={this.LoadEveryCountryData} className='button1'>Load Every Data from API</button>
+        <button onClick={this.ResetTableToInitialValue}>Erase data</button>
+      </div>
+
+      <Map data={this.state.data} countryList={this.state.countryList}/>
+
+      <div className="add">
+          <h2>Add data for a specific country:</h2>
+          <form>
+            <input type="text" name="name" required="required" placeholder='Enter a Country Name' onChange={this.handleAddCountry}/>
+            <input type="text" name="iso3" required="required" placeholder='Enter a Country Symbol' onChange={this.handleAddCountry}/>
+            <input type="number" name="confirmed" required="required" placeholder='number of confirmed cases' onChange={this.handleAddCovidData}/>
+            <input type="number" name="deaths" required="required" placeholder='number of deaths' onChange={this.handleAddCovidData}/>
+            <button onClick={this.handleAddData}>Add</button>
+          </form>
+      </div>
+
         <table>
           <thead>
             <tr>
@@ -65,6 +159,8 @@ render (){
               <th>Symbol</th>
               <th>Confirmed Covid cases</th>
               <th>deaths</th>
+              <th>Actions</th>
+
             </tr>
             </thead> 
           <tbody>
@@ -89,7 +185,7 @@ render (){
           </tbody>
         </table>
 
-        {console.log(this.state.data)}
+        {/* {console.log(this.state.data)} */}
         {/* {setTimeout(()=>this.state.data.map((d,i)=> <p key={i}>{d.deaths.value}</p>),5000)} */}
         {/* <p>{typeof(this.state.data[10].confirmed.value) !== 'undefined' && this.state.data[10] !== null &&
         this.state.data[10].confirmed.value}</p>  */}
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,8 +1,126 @@
-function Map() {
-    return ( 
-        <>
-        </>
-     );
-}
+//import dataCountries from '../Data/countries.geojson';
+import './Map.css';
+import { Component } from 'react';
+//import L from 'leaflet';
 
-export default Map;
+
+
+
+const style = {
+    width: "50%",
+    height: "40%"
+  };
+
+  /* const mapStyle = (feature) => {
+    return ({
+      weight: 2,
+      opacity: 1,
+      color: "white",
+      dashArray: "3",
+      fillOpacity: 0.7,
+      fillColor: getColor(feature.properties.density)
+    });
+  } */
+  
+ /*  const getColor = (d) =>{
+    return d > 1000
+    ? "#800026"
+    : d > 500
+      ? "#BD0026"
+      : d > 200
+        ? "#E31A1C"
+        : d > 100
+          ? "#FC4E2A"
+          : d > 50
+            ? "#FD8D3C"
+            : d > 20 ? "#FEB24C" : d > 10 ? "#FED976" : "#FFEDA0";
+  } */
+  class Map extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+          data: this.props.data,
+          countryList : this.props.countryList
+      }
+  }/* 
+    componentDidMount() {
+      // create map
+      //console.log(Object.keys(data.features[0].properties));
+      console.log(dataCountries);
+      this.map = L.map("map", {
+        center: 40,
+        zoom: 5,
+        layers: [
+          L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmxvcnZhbmRla2VyY2tob3ZlIiwiYSI6ImNqdGZyMmtrejAxYWw0M3A2OGtwdTMxNWEifQ.5U-KSDZfyKNC_Z74fEWj6g",
+          {
+            maxZoom: 15,
+            attribution:
+              'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: "streets-v9"
+          })
+        ]
+      });
+  
+      this.geojson = L.geoJson(dataCountries, {
+        style: mapStyle,
+        onEachFeature: this.onEachFeature
+      }).addTo(this.map);
+  
+      this.info = L.control();
+  
+      this.info.onAdd = function(map) {
+        this._div = L.DomUtil.create("div", "info");
+        this.update();
+        return this._div;
+      };
+  
+      this.info.update = function(props) {
+        this._div.innerHTML =
+          "<h4>Energy poverty in Portugal</h4>" +
+          (props
+            ? "<b>" +
+              props.name +
+              "</b><br />" +
+              props.density +
+              " people / mi<sup>2</sup>"
+            : "Hover over " + this.region);
+      };
+  
+      this.info.addTo(this.map);
+  
+      // add layer
+      this.layer = L.layerGroup().addTo(this.map);
+    }
+    onEachFeature = (feature, layer) => {
+      layer.on({
+        mouseover: this.highlightFeature,
+        mouseout: this.resetHighlight,
+        click: this.zoomToFeature
+      });
+    }
+    highlightFeature = (e) => {
+      var layer = e.target;
+      layer.setStyle({
+        weight: 5,
+        color: "#666",
+        dashArray: "",
+        fillOpacity: 0.7
+      });
+  
+      layer.bringToFront();
+  
+      this.info.update(layer.feature.properties);
+    }
+    resetHighlight = (event) => {
+      this.geojson.resetStyle(event.target);
+      this.info.update();
+    }
+    zoomToFeature = (e) => {
+      this.map.fitBounds(e.target.getBounds());
+    } */
+    render() {
+      return <div id="map" style={style}/>;
+      }
+  }
+  
+  export default Map;
