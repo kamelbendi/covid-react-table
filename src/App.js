f51016e3620a8cc49797ca0c27b169522a374ec6
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-11-18 12:36:35 +0100

    remouving table of data

--- src/App.js
+++ src/App.js
@@ -2,156 +2,183 @@ import { Component} from 'react';
 import './App.css';
 import Map from './Components/Map';
 import 'leaflet/dist/leaflet.css';
-import { nanoid } from '@reduxjs/toolkit';
+//import { nanoid } from '@reduxjs/toolkit';
+
 
 
 class App extends Component {
+  
   state = {
+    
     data: [{
-      "confirmed" : {"value": 300}, 
-      "deaths" :   {"value": 300} 
+      "confirmed" : {"value": 30000}, 
+      "deaths" :   {"value": 300000},
+      "name":"Algeria",
+      "iso3": "DZA",
+      'id': "12334dzds43"
     }],
-    countryList: {
-      "countries" : [{"name":"First Country", "iso3": "FC"}]
-    },
-    addData : {
-      "data" : [{"confirmed": {"value": 0}, "deaths": {"value": 0}}],
-      "countryList" : {"countries" : [{"name":"", "iso3":""}]}
-    }
+    
+    //addData :  [{"name": "", "iso3":"", "confirmed": {"value": 0}, "deaths": {"value": 0}}],
+       
+    //editContactId: {index : null, name:"", deaths: {"value": 0}, confirmed: {"value": 0}, iso3: ""}
   }
-  LoadEveryCountryData = () => {
-    this.setState({ countryList : []})
-    this.setState({ data: [] });
+  
+
+  LoadEveryCountryData = async () => {
+    
+    //this.setState({ data : [] })
+    
     //load all countries
-    fetch('https://covid19.mathdro.id/api/countries')
+
+    
+      fetch('https://covid19.mathdro.id/api/countries')
     .then((response) => response.json())
     .then(countryList => {
-        this.setState({ countryList: countryList });
-    });
-    //get corresponsing data
-    this.state.countryList.countries.map(country => this.getCountryData(country.name));
+      let filtered = countryList.countries.filter( country => typeof(country.iso3) !== 'undefined')
+      this.setState({data: filtered})});
+        //this.setState( {countryList : countryList});
+    
+
+         
+}
+
+  async componentDidUpdate(prevProps, prevState){
+if(this.state.data !== [] && prevState.data !== this.state.data){
+    await this.LoadEveryCountryData();
+      this.state.data.map( async (country, index) => {
+        fetch('https://covid19.mathdro.id/api/countries/' + country.name)
+           .then((response) => response.json())
+           .then( dataCovid => {
+             let items = this.state.data;
+             let item = {...items[index]};
+             if(dataCovid.deaths) {item.deaths =  {value : dataCovid.deaths.value};
+             item.confirmed = {value : dataCovid.confirmed.value};}
+             items[index] = item;
+             
+             this.setState({data : items})})
+             
+     //get corresponsing data
+     //this.state.data.map(data => this.getCountryData(this.state.data.name));
+     
+   })
+    }
   }
 
   ResetTableToInitialValue= () => {
     this.setState({ data: [] }); 
-    this.setState({countryList: {"countries":[]}});
   }
 
-  componentDidMount(){
-    
-  }
-// retrieving data from every country
 
- getCountryData = async (country) => {
-  const response = await fetch('https://covid19.mathdro.id/api/countries/' + country);
-  const result = response.json();
-  this.setState({data : [...this.state.data, await result]});
-}
-
-handleAddCovidData = (event) => {
+/*  handleAddCountry = (event) => {
   event.preventDefault();
   const fieldName = event.target.getAttribute('name');
   const fieldValue = event.target.value;
-
-
-if(fieldName==='confirmed'){
-  // 1. Make a shallow copy of the items
-  let addData = {...this.state.addData};
-  // 2. Make a shallow copy of the item you want to mutate
-  let data = {...addData.data[0]};
-  // 3. Replace the property you're intested in
-  data.confirmed.value = fieldValue;
-  // 4. Put it back into our array. N.B. we *are* mutating the array here, 
-  //    but that's why we made a copy first
-  addData.data[0] = data;
-  // 5. Set the state to our new copy
-  this.setState({addData: addData});
   
-}else if (fieldName==='deaths'){
-  let addData = {...this.state.addData};
-  // 2. Make a shallow copy of the item you want to mutate
-  let data = {...addData.data[0]};
-  // 3. Replace the property you're intested in
-  data.deaths.value = fieldValue;
-  // 4. Put it back into our array. N.B. we *are* mutating the array here, 
-  //    but that's why we made a copy first
-  addData.data[0] = data;
-  // 5. Set the state to our new copy
-  this.setState({addData: addData});
-}
+  const newFormData = {...this.state.addData}
+  newFormData[fieldName] = fieldValue;
+
+  this.setState({ addData : newFormData})
 
 }
 
-handleAddCountry = (event) => {
+handleAddCountrySubmit = (event) => {
   event.preventDefault();
-  const fieldName = event.target.getAttribute('name');
-  const fieldValue = event.target.value;
-
-
-  if(fieldName==='name'){
-    // 1. Make a shallow copy of the items
-    let addData = {...this.state.addData};
-    // 2. Make a shallow copy of the item you want to mutate
-    let data = {...addData.countryList.countries[0]};
-    // 3. Replace the property you're intested in
-    data.name = fieldValue;
-    // 4. Put it back into our array. N.B. we *are* mutating the array here, 
-    //    but that's why we made a copy first
-    addData.countryList.countries[0] = data;
-    // 5. Set the state to our new copy
-    this.setState({addData: addData});
-    
-  }else if (fieldName==='iso3'){
-    let addData = {...this.state.addData};
-    // 2. Make a shallow copy of the item you want to mutate
-    let data = {...addData.countryList.countries[0]};
-    // 3. Replace the property you're intested in
-    data.iso3 = fieldValue;
-    // 4. Put it back into our array. N.B. we *are* mutating the array here, 
-    //    but that's why we made a copy first
-    addData.countryList.countries[0] = data;
-    // 5. Set the state to our new copy
+  const newCountry = {
+    id: nanoid(),
+    name : this.state.addData.name,
+    iso3 : this.state.addData.iso3,
+    deaths : {value : this.state.addData.deaths},
+    confirmed : {value : this.state.addData.confirmed}
   }
-
-}
-handleAddData = (event) => {
-  event.preventDefault();
-
-  const dataArray = {"confirmed":{"value": this.state.addData.data[0].confirmed.value}, 'deaths': {"value": this.state.addData.data[0].deaths.value}, id: nanoid()};
-  const countryObject = {"name":this.state.addData.countryList.countries[0].name, "iso3": this.state.addData.countryList.countries[0].iso3}; 
-  this.setState({data: [...this.state.data, dataArray]})
-  this.setState({countryList: {"countries" : [...this.state.countryList.countries, countryObject]}});
-  console.log(this.state.addData);
+  const countries = [...this.state.data, newCountry];
+  this.setState({data : countries})
+} */
+  /* console.log(this.state.addData);
   console.log(this.state.data);
-  console.log(this.state.countryList);
+  console.log(this.state.countryList); */
 
   /* this.setState({addData: {
     "data" : [{"confirmed": 0, "deaths": 0}],
     "countryList" : {"countries" : [{"name":"", "iso3":""}]}
-  } }) */
+  } }) 
+  
+} */
+
+/* handleEditClick = (event, index) => {
+  event.preventDefault();
+  const country = this.state.data[index];
+  country.index = index;
+  this.setState({editContactId: country});
+}
+
+saveDataFromButton = (event, index) => {
+  event.preventDefault();
+  const edit = this.state.editContactId;
+  edit.index = null;
+  const countryList = this.state.data;
+  const country = countryList[index];
+  delete country.index;
+  country.name = edit.name
+  country.iso3 = edit.iso3
+  country.confirmed.value = edit.confirmed.value
+  country.deaths.value = edit.deaths.value
+  countryList[index] = country;
+  //const countryList = this.state.data.slice(0, index-1).concat(country, this.state.data.slice(index+1, this.state.data.length - 1 ))
+  this.setState({data: countryList})
+  this.setState({editContactId : edit})
   
 }
+
+handleCancelEdit = (event) => {
+  event.preventDefault();
+  const country = this.state.editContactId;
+  delete country.index;
+  this.setState({editContactId : country});
+}
+
+handleDeleteClick = (event, index) => {
+  event.preventDefault();
+  const countries = this.state.data.slice(0,index-1).concat(this.state.data.slice(index+1, this.state.data.length-1));
+  this.setState({data: countries});
+}
+
+handleInputChanges = (event) =>{
+  event.preventDefault();
+  const fieldName = event.target.getAttribute('name');
+  const fieldValue = event.target.value;
+  
+  const newFormData = {...this.state.editContactId}
+  if(fieldName==="iso3"|| fieldName === "name"){
+    newFormData[fieldName] = fieldValue;}
+  else{
+    newFormData[fieldName].value = fieldValue;
+  }
+
+  this.setState({ editContactId : newFormData})
+
+} */
+
 render (){
   return (
     <div className="app-container">
       <div className='buttons'>
-        <button onClick={this.LoadEveryCountryData} className='button1'>Load Every Data from API</button>
-        <button onClick={this.ResetTableToInitialValue}>Erase data</button>
+        <button onClick={this.LoadEveryCountryData} className='load-data'>Load Every Data from API</button>
+        <button onClick={this.ResetTableToInitialValue} className='erase-data'>Erase data</button>
       </div>
+      {console.log(this.state.data)}
+      <Map data={this.state.data}></Map>
 
-      <Map data={this.state.data} countryList={this.state.countryList}/>
-
-      <div className="add">
+      {/* <div className="add">
           <h2>Add data for a specific country:</h2>
-          <form>
+          <form onSubmit={this.handleAddCountrySubmit}>
             <input type="text" name="name" required="required" placeholder='Enter a Country Name' onChange={this.handleAddCountry}/>
-            <input type="text" name="iso3" required="required" placeholder='Enter a Country Symbol' onChange={this.handleAddCountry}/>
-            <input type="number" name="confirmed" required="required" placeholder='number of confirmed cases' onChange={this.handleAddCovidData}/>
-            <input type="number" name="deaths" required="required" placeholder='number of deaths' onChange={this.handleAddCovidData}/>
-            <button onClick={this.handleAddData}>Add</button>
+            <input type="text" name="iso3" required="required" placeholder='Enter a Country Symbol ISO_A3' onChange={this.handleAddCountry}/>
+            <input type="number" name="confirmed" required="required" placeholder='number of confirmed cases' onChange={this.handleAddCountry}/>
+            <input type="number" name="deaths" required="required" placeholder='number of deaths' onChange={this.handleAddCountry}/>
+            <button type="submit"  className='add-button'>Add</button>
           </form>
       </div>
-
+      <form>
         <table>
           <thead>
             <tr>
@@ -165,27 +192,55 @@ render (){
             </thead> 
           <tbody>
             
+
             {
-              this.state.data.map((d, index) => (
-                <tr key={index}>
-                  <td>{this.state.countryList.countries[index].name}</td>
-                  <td>{this.state.countryList.countries[index].iso3}</td>
-                  {typeof(d.deaths) !== 'undefined' && (<>
-                    <td>{d.confirmed.value}</td>
-                    <td>{d.deaths.value}</td>
-                  </>)
+              this.state.data.map((country, index) => (
+                <Fragment>
                   
-                  } 
-                  
+                { this.state.editContactId.index === index ? <tr key={index+300}>
+                    <td>
+                      <input name="name" required="required" placeholder='enter a country' type='text' value={this.state.editContactId.name} onChange={this.handleInputChanges}/>
+                    </td>
+                    <td>
+                      <input name="iso3" required="required" placeholder='enter a country' type='text' value={this.state.editContactId.iso3} onChange={this.handleInputChanges}/>
+                    </td>
+                    <td>
+                      <input name="confirmed" required="required" placeholder='enter a country' type='number' value={this.state.editContactId.confirmed.value} onChange={this.handleInputChanges}/>
+                    </td>
+                    <td>
+                      <input name="deaths" required="required" placeholder='enter a country' type='number' value={this.state.editContactId.deaths.value} onChange={this.handleInputChanges}/>
+                    </td>
+                    <td>
+                      <button onClick={(event) => this.saveDataFromButton(event, index)}>Save</button>
+                      <button onClick= {(event) => this.handleCancelEdit(event)}>Cancel</button>
+                    </td>
                 </tr>
+                : <tr key={index}>
+                    <td>{country.name}</td>
+                    <td>{country.iso3}</td>
+                  {typeof(country.confirmed) !== 'undefined' ? (<>
+                    <td>{country.confirmed.value}</td>
+                    <td>{country.deaths.value}</td>
+                  </>)
+                  :
+                  (<><td></td>
+                  <td></td></>)
+                  } 
+                  <td>
+                    <button onClick={(event) => this.handleEditClick(event, index)}>Edit</button>
+                    <button onClick={(event) => this.handleDeleteClick(event, index)}>Delete</button>
+                  </td>
+                </tr>}
+                
+          </Fragment>
               ))
-            } 
-            
-            
+            }
           </tbody>
         </table>
+        </form> */}
 
-        {/* {console.log(this.state.data)} */}
+        
+        
         {/* {setTimeout(()=>this.state.data.map((d,i)=> <p key={i}>{d.deaths.value}</p>),5000)} */}
         {/* <p>{typeof(this.state.data[10].confirmed.value) !== 'undefined' && this.state.data[10] !== null &&
         this.state.data[10].confirmed.value}</p>  */}
